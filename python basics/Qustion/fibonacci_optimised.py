ab6d7d354dd3853244365a080e43fd2f5a4efa9e
Talha Sayyed <talhasayyed1995@gmail.com>
2026-01-30 22:59:52 +0530

    Delete python basics/Qustion directory

--- python basics/Qustion/fibonacci_optimised.py
+++
@@ -1,5 +0,0 @@
-def fibonacci(n: int) -> int:
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a+b
-    return a
--- python basics/Qustion/fibonacci_recursion.py
+++
@@ -1,4 +0,0 @@
-def fibonacci_r(n: int): -> int
-    if n<=1:
-        return n
-    return fibonacci_r(n-1) + fibonacci_r(n-2)
